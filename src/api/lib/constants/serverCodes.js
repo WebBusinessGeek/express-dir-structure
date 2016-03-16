cc868fcf42bfef6d31b241aa18023398bd14ca8a
Kevin <kevw12188@gmail.com>
2016-03-15 21:27:47 -0400

    init

---
+++ src/api/lib/constants/serverCodes.js
@@ -0,0 +1,5 @@
+module.exports = {
+    SUCCESS_CODE : 200,
+    FAILURE_CODE : 400,
+    ERROR_CODE : 500
+}
---
+++ src/api/lib/http/routerFactory.js
@@ -0,0 +1,5 @@
+import express from "express"
+
+export default function newRouter() {
+    return express.Router()
+}
